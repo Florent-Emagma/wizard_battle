e3f890479195b4a60ecfd3de39d9646e8f18e3af
Florent <florent.vite@emagma.fr>
2017-08-11 11:47:21 +0200

    Ajout configuration BDD

--- functions.php
+++ functions.php
@@ -1,2 +1,9 @@
 <?php
+
 require __DIR__.'/vendor/autoload.php';
+
+$configuration = [
+    'db_dsn' => 'mysql:host=127.0.0.1;dbname=wizard_battle',
+    'db_user' => 'root',
+    'db_password' => '',
+];
